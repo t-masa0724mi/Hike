ee2077c1dd674e692211970c7798b48b3ebdf537
masa950724mi <s100.iizuka.masami@gmail.com>
2023-06-07 11:59:11 +0900

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by 飯塚政美 on 2023/06/07.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
